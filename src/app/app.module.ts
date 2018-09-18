ce286c5e54a314126908c2d727f9839594ee70d7
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2018-09-18 21:48:16 +0630

    boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,16 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  declarations: [MyApp],
+  entryComponents: [MyApp],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+  providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }, SplashScreen, StatusBar],
+  bootstrap: [IonicApp]
 })
 export class AppModule {}
