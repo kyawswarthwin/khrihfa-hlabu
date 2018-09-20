41656d18e9760ea5de12227362dd41625d4f7684
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2018-09-20 13:02:49 +0630

    add pipe

--- src/pages/hymn/hymn.module.ts
+++ src/pages/hymn/hymn.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { HymnPage } from './hymn';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
-  declarations: [
-    HymnPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(HymnPage),
-  ],
+  declarations: [HymnPage],
+  imports: [IonicPageModule.forChild(HymnPage), PipesModule]
 })
 export class HymnPageModule {}
---
+++ src/pipes/pipes.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { SafePipe } from './safe/safe';
+
+@NgModule({
+  declarations: [SafePipe],
+  imports: [],
+  exports: [SafePipe]
+})
+export class PipesModule {}
